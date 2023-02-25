1be10e85e42bae9850685a55f9d0d5e946999be6
Scriptinfano <2996296637@qq.com>
2023-02-25 10:34:59 +0800

    无

--- data_structure_experiment/hashTable/HashTable.c
+++ data_structure_experiment/hashTable/HashTable.c
@@ -7,7 +7,6 @@ const int examples = 10;//测试样本数
 const int max = 10;//随机样本数的最大值
 const int min = 1;//随机样本数的最小值
 const int hashTableSize = 5;//哈希表表长
-#define MAXLENGTH 10 //字符串的最大长度
 
 //返回随机的不重复的键值数组，第一个参数是返回数组的大小，第二个参数是生成随机数的最小值，第三个参数是生成随机数的最大值
 int *createRandomArray(int size, int minNum, int maxNum) {
@@ -30,6 +29,12 @@ int *createRandomArray(int size, int minNum, int maxNum) {
     return randomArray;
 }
 
+void hashSearchTest(Table *theTable);
+
+bool checkKey(int key);
+
+void elementDeleteTest(Table *theTable);
+
 int main() {
     setbuf(stdout, NULL);//调试时将数据输出到控制台
     srand((unsigned int) time(NULL));//根据时间提供随机数种子
@@ -40,43 +45,83 @@ int main() {
     //开辟字符串数组空间，录入待插入的字符串，这些字符串作为待插入哈希表对象的值
     char **str = (char **) malloc(sizeof(char *) * examples);
     //以下是待测试的字符串，仅在测试时保留，一般情况下采用用户输入的方法
-    str[0]="apple";
-    str[1]="banana";
-    str[2]="car";
-    str[3]="truck";
-    str[4]="rubbish";
-    str[5]="rabbit";
-    str[6]="like";
-    str[7]="mike";
-    str[8]="sense";
-    str[9]="fine";
+    str[0] = "apple";
+    str[1] = "banana";
+    str[2] = "car";
+    str[3] = "truck";
+    str[4] = "rubbish";
+    str[5] = "rabbit";
+    str[6] = "like";
+    str[7] = "mike";
+    str[8] = "sense";
+    str[9] = "fine";
     /*用户输入的方式录入字符串
     printf("依次输入每一个数据项的字符串数据：");
     for(int i = 0; i < examples; i++){
         gets(str[i]);
     }
     */
+
     printf("输出生成的关键字与相应的数据：");
     for (int i = 0; i < examples; i++) {
         hashInsert(theTable, randomArray[i], str[i]);//将由整型键值和字符型值的待插入哈希表对象插入哈希表
         printf("(%d,%s) ", randomArray[i], str[i]);
     }
-    printf("\n");
 
+    printf("\n");
+    printf("测试首次哈希查找\n");
+    hashSearchTest(theTable);
+    printf("测试删除哈希元素\n");
+    elementDeleteTest(theTable);
+    printf("测试删除元素之后的哈希查找");
+    hashSearchTest(theTable);
+
+    return 0;
+}
+
+void hashSearchTest(Table *theTable) {
     //开始哈希查找
     while (true) {
         printf("输入你要查找的节点的关键字（%d到%d）,输入-1结束输入:", min, max);
         int theNumber = 0;
         scanf("%d", &theNumber);
         getchar();//吞掉换行符
+        if (!checkKey(theNumber)) {
+            printf("你输入的关键字不在范围之内，请重新输入");
+            continue;
+        }
         if (theNumber == -1) {
             break;
-        }else {
-            Node *searchPointer=hashSearch(theTable, theNumber);
+        } else {
+            Node *searchPointer = hashSearch(theTable, theNumber);
             if (searchPointer) {
-                printf("找到了，其值为%s\n",searchPointer->info);
+                printf("找到了，其值为%s\n", searchPointer->info);
             } else printf("未找到\n");
         }
     }
-    return 0;
+}
+
+void elementDeleteTest(Table *theTable) {
+    while (true) {
+        printf("输入你要删除的节点的关键字（%d到%d）,输入-1结束输入:", min, max);
+        int theNumber = 0;
+        scanf("%d", &theNumber);
+        getchar();//吞掉换行符
+        if (!checkKey(theNumber)) {
+            printf("你输入的关键字不在范围之内，请重新输入");
+            continue;
+        }
+        if (theNumber == -1) {
+            break;
+        } else {
+            if (hashElementDelete(theTable, theNumber)) {
+                printf("删除成功\n");
+            } else printf("未找到待删除元素\n");
+        }
+    }
+}
+//检查输入的关键字是否在范围之内，如果不在范围之内则返回false
+bool checkKey(int key) {
+    if(key==-1)return true;//-1是特例，说明用户要结束输入
+    return !(key < min || key > max);
 }
--- data_structure_experiment/hashTable/HashTable.h
+++ data_structure_experiment/hashTable/HashTable.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+
 typedef int KeyType;
 typedef char *InfoType;
 typedef struct HashNode {
@@ -66,62 +67,76 @@ void hashInsert(Table *theTable, KeyType theKey, InfoType theInfo) {
     theTable->elementSize++;//节点数更新
 }
 
-//哈希表元素的查找，若找到了则返回该元素的前一个节点的地址，没找到则返回空地址
-/*Node *hashSearch(Table *theTable, Node *theNode) {
-    KeyType theKey = theNode->key;
-    InfoType theInfo = theNode->info;
-    int address = hashFunction(theTable, theKey);//利用哈希映射根据关键字计算存储位置
-    if (address < 0 || address >= theTable->tableLength) {
-        //关键字经过哈希映射之后找不到位置
-        return NULL;
-    }
-    Node *pointer = theTable->table[address];
-    if (pointer == NULL) {
-        return NULL;
-    }
-    Node *prePointer = pointer->pre;
+//输入节点的key值，返回该节点所在链表的头部节点指针
+Node *headPointer(Table *theTable, KeyType theKey) {
+    int address = hashFunction(theTable, theKey);
+    if (address < 0)return NULL;
+    return theTable->table[address];
+}
 
-    do {
-//比较有问题
-        if (strcmp(theInfo,pointer->info)==0) {
-            return prePointer;
-        }
-        prePointer=pointer;
-        pointer = pointer->next;
-    } while (pointer != theTable->table[address]);
-    return NULL;
-
-}*/
+///输入节点的key值，返回该节点所在返回尾部节点指针
+Node *tailPointer(Table *theTable, KeyType theKey) {
+    int address = hashFunction(theTable, theKey);
+    if (address < 0)return NULL;
+    return theTable->table[address]->pre;
+}
 
 //哈希查找指定的节点，返回找到的节点的指针，如果没有找到则返回NULL，
-Node* hashSearch(Table *theTable,KeyType theKey){
+Node *hashSearch(Table *theTable, KeyType theKey) {
     int address = hashFunction(theTable, theKey);//利用哈希映射根据关键字计算存储位置
-    if(address<0)return NULL;//除留余数法当除数是负数的时候，不能保证结果会落在指定区间内，所以如果用户输入了负数应该直接返回NULL
+    if (address < 0)return NULL;//除留余数法当除数是负数的时候，不能保证结果会落在指定区间内，所以如果用户输入了负数应该直接返回NULL
     Node *pointer = theTable->table[address];
     if (pointer == NULL) {
         return NULL;
-    }else{
-        //顺着该链表依次向后对比存储的元素的键值是否和参数键值相同
-        do{
-            if(theKey==pointer->key)
+    } else {
+        //遍历该双向循环链表，比对键值，找到了就返回节点指针
+        while (pointer != NULL) {
+            if (theKey == pointer->key)
                 return pointer;
-            else{
-                pointer=pointer->next;
+            else {
+                pointer = pointer->next;
             }
-        }while(pointer!=theTable->table[address]->pre);//TODO 此处bug待修改
-        return NULL;//在该单链表中遍历之后，发现没有和参数键值相同的节点，说明查找失败了
+            if (pointer == theTable->table[address])
+                break;
+        }
+        return NULL;
     }
 }
 
 //哈希表元素的删除，删除成功返回true，失败则返回false
 bool hashElementDelete(Table *theTable, KeyType theKey) {
-    Node *rear = hashSearch(theTable, theKey);
-    if (rear) {
-        //找到了该元素，则删除该元素
-        Node *temp = rear->next;
-        rear->next = rear->next->next;
-        rear->next->pre = rear;
-        free(temp);
+    Node *deleteNode = hashSearch(theTable, theKey);
+    Node *head = headPointer(theTable, theKey);
+    Node *tail = tailPointer(theTable, theKey);
+    if (deleteNode) {
+        //如果待删除的节点所在的链表只有待删除节点一个节点，则仅删除该节点，然后将哈希表中该节点所在的槽位置空
+        if (deleteNode->next == deleteNode) {
+            //此时链表中只有一个节点
+            free(deleteNode);
+            theTable->table[hashFunction(theTable, theKey)] = NULL;
+            return true;
+        }
+        //找到了该元素，则删除该元素，以下均讨论链表中有多个节点的情况
+        if (deleteNode == head) {
+            //链表中有多个节点，删除第一个节点
+            Node *temp = deleteNode;
+            Node *newHead = deleteNode->next;
+            newHead->pre = tail;
+            tail->next = newHead;
+            theTable->table[hashFunction(theTable, theKey)] = newHead;
+            free(temp);
+        } else if (deleteNode == tail) {
+            //如果待删除的节点是链表的最后一个节点
+            deleteNode->pre->next = head;
+            head->pre = deleteNode->pre;
+            free(deleteNode);
+        } else {
+            //如果待删除的节点是链表中间的某个节点
+            deleteNode->pre->next = deleteNode->next;
+            deleteNode->next->pre = deleteNode->pre;
+            free(deleteNode);
+        }
+        theTable->elementSize--;
         return true;
     } else return false;
 }
@@ -134,14 +149,14 @@ void hashTableDelete(Table *theTable) {
         pointer = theTable->table[i]->pre;//pointer指向链表的最后一个节点
         nextPointer = pointer->pre;//nextPointer指向倒数第二个节点
         theTable->table[i]->pre = NULL;
-        theTable->table[i]=NULL;
+        theTable->table[i] = NULL;
         while (nextPointer != NULL) {
             free(pointer);
             pointer = nextPointer;
             nextPointer = nextPointer->pre;
         }
         free(pointer);
-        pointer=NULL;
+        pointer = NULL;
     }
     free(theTable->table);
 }
